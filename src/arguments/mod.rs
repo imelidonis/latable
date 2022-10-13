b98f61a558769acb72de0162e1bbf137734c9400
imel <melidonis00@gmail.com>
2022-10-13 14:37:34 +0300

    Column Definition validation and calculation

--- src/arguments/mod.rs
+++ src/arguments/mod.rs
@@ -7,11 +7,11 @@ use clap::Parser;
 pub struct LatableArgs {
     /// Number of rows
     #[clap(short = 'r', long = "rows")]
-    rows: u32,
+    rows: usize,
     
     /// Number of columns
     #[clap(short = 'c', long = "columns")]
-    columns: u32,
+    columns: usize,
 
     /// Column Definition, eg. 'ccc' for 3 centered columns. Default: all columns centered
     /// Accepted options: l (left-align), c (center-align), r (right - align).
@@ -24,9 +24,44 @@ pub struct LatableArgs {
     column_def: Option<String>,
 }
 
+impl LatableArgs {
+    pub fn get_column_def(&self) -> Option<String> {
+        self.column_def.clone()
+    }
+
+    pub fn get_columns(&self) -> usize {
+        self.columns
+    }
+
+    #[allow(dead_code)]
+    pub fn get_rows(&self) -> usize {
+        self.rows
+    }
+
+    pub fn has_col_def(&self) -> bool {
+        self.column_def.is_some()
+    }
+
+    pub fn col_def_size_validation(&self) -> Result<(), String> {
+        match &self.column_def {
+            // The column definition should contain the same number of columns
+            Some(col_def) => if col_def.chars().count() == self.columns {
+                Ok(())
+            } else {
+                Err(format!(
+                    "The column definition should contain the same number of columns. Expected: {} found: {}",
+                    self.columns,
+                    col_def.chars().count()
+                ))
+            },
+            None => Err("Column Definition not found".to_string())
+        }
+    }
+}
+
 fn column_def_validation(s: &str) -> Result<String, String> {
     if s.chars().any(|c| c != 'l' && c != 'c' && c != 'r') {
-        Err(String::from("Column Definition can only contains: l, c or r]"))
+        Err(String::from("Column Definition can contain only: l, c or r"))
     } else {
         Ok(String::from(s))
     }
--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,21 @@ use clap::Parser;
 fn main() {
     let args = LatableArgs::parse();
 
+    if args.has_col_def() {
+        match args.col_def_size_validation() {
+            Ok(_) => (),
+            Err(msg) => {
+                eprintln!("error: {}", msg);
+                std::process::exit(1);
+            }
+        }
+    }
+
+    let col_def = args.get_column_def().unwrap_or_else(
+        // if column definition wasn't provided, all columns are centered.
+        || "c".repeat(args.get_columns())
+    );
+
     println!("{:?}", args);
+    println!("{}", col_def);
 }
